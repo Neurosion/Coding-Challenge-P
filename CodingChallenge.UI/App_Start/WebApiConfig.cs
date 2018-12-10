a49f104828002ffdc4af0287774124d0f0b64722
Neurosion <dissolutionapex@gmail.com>
2018-12-09 22:08:49 -0600

    Create infrastructure for web UI

---
+++ CodingChallenge.UI/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace CodingChallenge.UI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Use camel case for JSON data.
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
