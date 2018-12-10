ee23348aade9ec7c37565537d690f3a042127021
Neurosion <dissolutionapex@gmail.com>
2018-12-09 22:26:33 -0600

    Include dependents in employee entry

---
+++ CodingChallenge.UI/Scripts/app/Dependent.js
@@ -0,0 +1,14 @@
+﻿define(function (require) {
+    var ko = require("knockout");
+
+    function Dependent() {
+        var self = this;
+
+        self.firstName = ko.observable();
+        self.lastName = ko.observable();
+
+        return self;
+    };
+
+    return Dependent;
+});
--- CodingChallenge.UI/Scripts/app/Employee.js
+++ CodingChallenge.UI/Scripts/app/Employee.js
@@ -1,11 +1,22 @@
 ﻿define(function (require) {
     var ko = require("knockout");
+    var Dependent = require("Dependent");
 
     function Employee() {
         var self = this;
 
         self.firstName = ko.observable();
         self.lastName = ko.observable();
+        self.dependents = ko.observableArray();
+        self.hasDependents = ko.computed(function() {
+            return self.dependents().length > 0;
+        });
+        self.doesNotHaveDependents = ko.computed(function () {
+            return !self.hasDependents();
+        });
+        self.addDependent = function () {
+            self.dependents.push(new Dependent());
+        };
         return self;
     };
 
