a72d92b3a81fe157fd42d37a64be0cd2401861f9
Neurosion <dissolutionapex@gmail.com>
2018-12-09 21:59:40 -0600

    Add models for employees and dependents

---
+++ CodingChallenge.Core/People/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CodingChallenge.Core.People
+{
+    public class Employee : Person
+    {
+        public List<Person> Dependents { get; set; }
+    }
+}
---
+++ CodingChallenge.Core/People/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace CodingChallenge.Core.People
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public bool IsValid()
+        {
+            var result = !string.IsNullOrWhiteSpace(FirstName)
+                      || !string.IsNullOrWhiteSpace(LastName);
+
+            return result;
+        }
+    }
+}
