71396cb643008ba218480dbd10f1200e47e65ec3
Neurosion <dissolutionapex@gmail.com>
2018-12-09 22:03:06 -0600

    Calculate employee/dependent benefits and include discounts

---
+++ CodingChallenge.Core.Tests/Calculators/BenefitsWithDiscountsCalculatorTests.cs
@@ -0,0 +1,310 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CodingChallenge.Core.Calculators;
+using CodingChallenge.Core.Extensions;
+using CodingChallenge.Core.People;
+using NUnit.Framework;
+using Should;
+
+namespace CodingChallenge.Core.Tests.Calculators
+{
+    public class BenefitsWithDiscountsCalculatorTests
+    {
+        [Test]
+        public void Calculate_WhenProvidedNull_ReturnsEmpty()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+
+            var result = calculator.Calculate(null);
+
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void Calculate_WhenProvidedEmployee_WithNonEmptyName_ReturnsStandardBenefitDeduction()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "first",
+                LastName = "name"
+            };
+
+            var result = calculator.Calculate(employee).ToList();
+
+            var standardBenefit = result.FirstOrDefault(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.EmployeeStandardBenefitDeduction);
+
+            standardBenefit.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void Calculate_WhenProvidedEmployee_WithEmptyName_DoesNotReturnStandardBenefitDeduction()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee();
+
+            var result = calculator.Calculate(employee).ToList();
+
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void Calculate_WhenEmployeeElegibleForLetterADiscount_ReturnsNameDiscount()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "Alan"
+            };
+
+            var result = calculator.Calculate(employee).ToList();
+
+            var standardBenefit = result.FirstOrDefault(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.NameStartsWithADiscount);
+
+            standardBenefit.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void Calculate_WhenEmployeeNotElegibleForLetterADiscount_DoesNotReturnNameDiscount()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "Bill"
+            };
+
+            var result = calculator.Calculate(employee).ToList();
+
+            var standardBenefit = result.FirstOrDefault(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.NameStartsWithADiscount);
+
+            standardBenefit.ShouldBeNull();
+        }
+
+        [Test]
+        public void Calculate_WhenEmployeeHasDependents_AndNamesAreNotEmpty_ReturnsBenefitDeductionPerDependent()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "Bill",
+                Dependents = new List<Person>
+                {
+                    new Person { FirstName = "John" },
+                    new Person { FirstName = "Jill" }
+                }
+            };
+
+            var result = calculator.Calculate(employee);
+
+            var dependentDeduction = result.Where(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.DependentStandardBenefitDeduction).ToList();
+
+            dependentDeduction.Count.ShouldEqual(employee.Dependents.Count);
+        }
+
+        [Test]
+        public void Calculate_WhenEmployeeHasDependents_AndNamesAreEmpty_DoesNotReturnBenefitDeductionPerDependent()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "Bill",
+                Dependents = new List<Person>
+                {
+                    new Person(),
+                    new Person()
+                }
+            };
+
+            var result = calculator.Calculate(employee);
+
+            var dependentDeduction = result.Where(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.DependentStandardBenefitDeduction).ToList();
+
+            dependentDeduction.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void Calculate_WhenDependentElegibleForLetterADiscount_ReturnsNameDiscount()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "Bill",
+                Dependents = new List<Person>
+                {
+                    new Person
+                    {
+                        FirstName = "Alice"
+                    }
+                }
+            };
+
+            var result = calculator.Calculate(employee);
+
+            var nameDiscount = result.Where(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.NameStartsWithADiscount).ToList();
+
+            nameDiscount.ShouldNotBeNull();
+        }
+
+        [Test]
+        public void Calculate_WhenNameStartsWithADiscountIsAvailable_ReturnsLineItemsWithCorrectTotal()
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+            var employee = new Employee
+            {
+                FirstName = "Ann"
+            };
+
+            var result = calculator.Calculate(employee).ToList();
+
+            var expectedBenefitDeduction = -BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction;
+            var expectedNameDiscountPerAnnum = expectedBenefitDeduction * BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier;
+            var expectedNameDiscountPerPayPeriod = expectedNameDiscountPerAnnum.FromAnnumToPayPeriods(Constants.NumberOfPayPeriodsPerYear).ToList();
+            var nameDiscount = result.FirstOrDefault(x => x.Description == BenefitsWithDiscountsCalculator.Descriptions.NameStartsWithADiscount);
+            nameDiscount.PerAnnum.ShouldEqual(expectedNameDiscountPerAnnum);
+            
+            nameDiscount.PerPayPeriod.Count.ShouldEqual(expectedNameDiscountPerPayPeriod.Count);
+
+            for (var i = 0; i < expectedNameDiscountPerPayPeriod.Count; i++)
+                nameDiscount.PerPayPeriod[i].ShouldEqual(expectedNameDiscountPerPayPeriod[i]);
+        }
+
+        public static IEnumerable<object[]> DeductionTotalTestCases
+        {
+            get
+            {
+                // employee, no dependents
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Sally"
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                };
+
+                // employee with name discount, no dependents
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Albert"
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction 
+                  - BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier*BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                };
+
+                // employee, dependents
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Jim",
+                        Dependents = new List<Person>
+                        {
+                            new Person
+                            {
+                                FirstName = "Ellen"
+                            }
+                        }
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  + BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                };
+
+                // employee with name discount, dependents
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Ajax",
+                        Dependents = new List<Person>
+                        {
+                            new Person
+                            {
+                                FirstName = "Jill"
+                            }
+                        }
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  - BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier*BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  + BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                };
+
+                // employee with name discount, dependents with name discount
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Ajax",
+                        Dependents = new List<Person>
+                        {
+                            new Person
+                            {
+                                FirstName = "Aaron"
+                            }
+                        }
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  - BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier*BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  + BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                  - BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier*BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                };
+
+
+                // employee, dependents with name discount
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Tim",
+                        Dependents = new List<Person>
+                        {
+                            new Person
+                            {
+                                FirstName = "Aaron"
+                            }
+                        }
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  + BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                  - BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier*BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                };
+
+                // employee, some dependents with name discount
+                yield return new object[]
+                {
+                    new Employee
+                    {
+                        FirstName = "Bill",
+                        Dependents = new List<Person>
+                        {
+                            new Person
+                            {
+                                FirstName = "Iris"
+                            },
+                            new Person
+                            {
+                                FirstName = "Aaron"
+                            }
+                        }
+                    },
+                    BenefitsWithDiscountsCalculator.EmployeeBenefitDeduction
+                  + BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                  + BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                  - BenefitsWithDiscountsCalculator.NameStartsWithLetterADiscountMultiplier*BenefitsWithDiscountsCalculator.DependentBenefitDeduction
+                };
+            }
+        }
+
+        [TestCaseSource(nameof(DeductionTotalTestCases))]
+        public void Calculate_WhenProvidedEmployeesAndOrDependents_ReturnsExpectedTotal(Employee employee, decimal expectedTotal)
+        {
+            var calculator = new BenefitsWithDiscountsCalculator();
+
+            var result = calculator.Calculate(employee).ToList();
+            var actualTotal = result.Sum(x => x.PerAnnum);
+
+            actualTotal.ShouldEqual(expectedTotal);
+        }
+    }
+}
---
+++ CodingChallenge.Core/Calculators/BenefitsWithDiscountsCalculator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodingChallenge.Core.Extensions;
+using CodingChallenge.Core.People;
+
+namespace CodingChallenge.Core.Calculators
+{
+    public class BenefitsWithDiscountsCalculator : ICostCalculator<Employee>
+    {
+        public const decimal EmployeeBenefitDeduction = -1000m;
+        public const decimal DependentBenefitDeduction = -500m;
+        public const decimal NameStartsWithLetterADiscountMultiplier = .1m;
+
+        public static class Descriptions
+        {
+            public const string EmployeeStandardBenefitDeduction = "Standard benefit deduction for employee";
+            public const string DependentStandardBenefitDeduction = "Standard benefit deduction for dependent";
+            public const string NameStartsWithADiscount = "Discount for name starting with 'A'";
+        }
+
+        public IEnumerable<LineItem> Calculate(Employee target)
+        {
+            var benefits = GetEmployeeBenefits(target)
+                .Concat(GetDependentBenefits(target));
+
+            return benefits;
+        }
+
+        private static IEnumerable<LineItem> GetEmployeeBenefits(Employee employee)
+        {
+            if (!employee?.IsValid() ?? true)
+                yield break;
+
+            var employeeBenefit = new LineItem
+            {
+                Description = Descriptions.EmployeeStandardBenefitDeduction,
+                PerAnnum = EmployeeBenefitDeduction,
+                PerPayPeriod = EmployeeBenefitDeduction.FromAnnumToPayPeriods(Constants.NumberOfPayPeriodsPerYear).ToList()
+            };
+            yield return employeeBenefit;
+
+            var discount = GetDiscount(employeeBenefit, employee);
+
+            if (discount != null)
+                yield return discount;
+        }
+
+        private static IEnumerable<LineItem> GetDependentBenefits(Employee employee)
+        {
+            if (!employee?.Dependents?.Any() ?? true)
+                yield break;
+
+            foreach (var currentDependent in employee.Dependents)
+            {
+                if (!currentDependent?.IsValid() ?? true)
+                    continue;
+
+                var dependentBenefit = new LineItem
+                {
+                    Description = Descriptions.DependentStandardBenefitDeduction,
+                    PerAnnum = DependentBenefitDeduction,
+                    PerPayPeriod = DependentBenefitDeduction.FromAnnumToPayPeriods(Constants.NumberOfPayPeriodsPerYear).ToList()
+                };
+
+                yield return dependentBenefit;
+
+                var discount = GetDiscount(dependentBenefit, currentDependent);
+
+                if (discount != null)
+                    yield return discount;
+            }
+        }
+
+        private static LineItem GetDiscount(LineItem lineItem, Person person)
+        {
+            if (!person?.FirstName?.StartsWith("A", StringComparison.CurrentCultureIgnoreCase) ?? true)
+                return null;
+
+            var discountPerAnnum = lineItem.PerAnnum * -NameStartsWithLetterADiscountMultiplier;
+            var result = new LineItem
+            {
+                Description = Descriptions.NameStartsWithADiscount,
+                PerAnnum = discountPerAnnum,
+                PerPayPeriod = discountPerAnnum.FromAnnumToPayPeriods(Constants.NumberOfPayPeriodsPerYear).ToList()
+            };
+
+            return result;
+        }
+    }
+}
